67e56e39a494126720b4d883960716fae88fdee7
yportne <yportne@163.com>
2024-08-10 00:02:41 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+pub mod nbe_closure;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Term {
+    Idx(usize),
+    Lam(Box<Term>),
+    App(Box<Term>, Box<Term>),
+}
+
+fn lam(body: Term) -> Term {
+    Term::Lam(Box::new(body))
+}
+
+fn apply(f: Term, args: Vec<Term>) -> Term {
+    args.into_iter().fold(f, |acc, a| Term::App(Box::new(acc), Box::new(a)))
+}
+
+/// let rec church_aux = function
+///    | 0 -> Idx 0
+///    | n -> App(Idx 1, church_aux (n - 1))
+fn church_aux(n: usize) -> Term {
+    match n {
+        0 => Term::Idx(0),
+        _ => Term::App(Box::new(Term::Idx(1)), Box::new(church_aux(n - 1))),
+    }
+}
+
+fn church(n: usize) -> Term {
+    Term::Lam(Box::new(Term::Lam(Box::new(church_aux(n)))))
+}
+
+fn church_add() -> Term {
+    lam(
+        lam(
+            lam(
+                lam(
+                    apply(
+                        Term::Idx(3),
+                        vec![Term::Idx(1), apply(Term::Idx(2), vec![Term::Idx(1), Term::Idx(0)])])
+                )
+            )
+        )
+    )
+}
+
+fn main() {
+    //println!("Hello, world!");
+    let i = 1000;
+    let a = church(i);
+    let b = church(i);
+    let add = apply(church_add(), vec![a, b]);
+    let start = std::time::Instant::now();
+    let result = nbe_closure::normalize(add);
+    let end = start.elapsed();
+    println!("{:?}s", end.as_secs_f64());
+    let check = church(i + i);
+    println!("{}", result == check);
+}
---
+++ src/nbe_closure.rs
@@ -0,0 +1,62 @@
+use crate::Term;
+
+
+#[derive(Debug, Clone)]
+enum Value {
+    Lvl(usize),
+    Lam(Vec<Value>, Term),
+    App(Box<Value>, Box<Value>),
+}
+
+
+
+/// eval env tm =
+///      match tm with
+///      | Idx idx   -> List.nth env idx
+///      | Lam tm'   -> VLam(env, tm')
+///      | App(f, a) -> apply_val (eval env f) (eval env a)
+fn eval(env: Vec<Value>, tm: Term) -> Value {
+    match tm {
+        Term::Idx(idx) => env[idx].clone(),
+        Term::Lam(tm) => Value::Lam(env, *tm),
+        Term::App(f, a) => apply_val(eval(env.clone(), *f), eval(env, *a)),
+    }
+}
+
+/// apply_val vf va =
+///      match vf with
+///      | VLam(env, body) -> eval (va :: env) body
+///      | _               -> VApp(vf, va)
+fn apply_val(vf: Value, va: Value) -> Value {
+    match vf {
+        Value::Lam(env, body) => eval([vec![va], env.to_vec()].concat(), body),
+        _ => Value::App(Box::new(vf), Box::new(va)),
+    }
+}
+
+/// quote level value =
+///      match value with
+///      | VLvl lvl        -> Idx(level - lvl - 1)
+///      | VLam(env, body) -> Lam(quote (level + 1) @@ eval (VLvl level :: env) body)
+///      | VApp(vf, va)    -> App(quote level vf, quote level va)
+fn quote(level: usize, value: Value) -> Term {
+    match value {
+        Value::Lvl(lvl) => Term::Idx(level - lvl - 1),
+        Value::Lam(env, body) => Term::Lam(
+            Box::new(
+                quote(
+                    level + 1,
+                    eval([vec![Value::Lvl(level)], env.to_vec()].concat(), body)
+                )
+            )
+        ),
+        Value::App(vf, va) => Term::App(
+            Box::new(quote(level, *vf)),
+            Box::new(quote(level, *va))
+        ),
+    }
+}
+
+pub fn normalize(t: Term) -> Term {
+    quote(0, eval(vec![], t))
+}
